b0f13e407e0524db3736154fdcfcd5b1a281299e
Doug Hoyte <doug@hcsw.org>
2019-11-08 21:40:30 -0500

    added initial contract sketch

---
+++ contract/Overlay.sol
@@ -0,0 +1,22 @@
+pragma solidity ^0.5.0;
+
+contract Overlay {
+    event LogRegister(address indexed sender, bytes32 content);
+
+    mapping(address => bytes32) private registry;
+
+    function register(bytes32 content) external {
+        registry[msg.sender] = content;
+        emit LogRegister(msg.sender, content);
+    }
+
+    function lookup(address[] memory addrs) public view returns (bytes32[] memory) {
+        bytes32[] memory output = new bytes32[](addrs.length);
+
+        for (uint i = 0; i < addrs.length; i++) {
+            output[i] = registry[addrs[i]];
+        }
+
+        return output;
+    }
+}
